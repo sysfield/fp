cd697135bdb76c8add08a9814ae7449db0654293
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-24 02:23:22 +0100

    Fix screen_size to be obtained on load

--- Enemy.gd
+++ Enemy.gd
@@ -1,11 +1,11 @@
 extends Sprite2D
 
 var speed = 100
-var screen_size = get_viewport_rect().size
+var screen_size
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
