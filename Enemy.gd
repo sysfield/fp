f3442d800f7a71cfee02697bb050f199d3d5283f
sysfield <139412420+sysfield@users.noreply.github.com>
2023-09-24 02:28:20 +0100

    Force sprite to stop at edge of screen on x axis

--- Enemy.gd
+++ Enemy.gd
@@ -1,14 +1,18 @@
 extends Sprite2D
 
-var speed = 100
+var speed = 400
 var screen_size
+var sprite_x
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	# https://ask.godotengine.org/71609/clamp-2d-not-working-in-main-scene
+	sprite_x = self.texture.get_width() / 2
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.position.x += speed * delta
-	self.position.clamp(Vector2.ZERO, screen_size)
+	position.x += speed * delta
+	position.x = clamp(position.x, 0, screen_size.x - sprite_x)
+	position.y = clamp(position.y, 0, screen_size.y)
